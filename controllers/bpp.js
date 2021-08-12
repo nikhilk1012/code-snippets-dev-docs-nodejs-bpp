81a81b4e1561d7873edbed007e3ad65172156baf
Nikhil Sharma <nikhil.k1012@gmail.com>
2021-08-12 12:31:10 +0530

    Updating Code

--- controllers/bpp.js
+++ controllers/bpp.js
@@ -11,56 +11,7 @@ const bpp = require("../services/bpp");
  */
 const search = ({ body }, res, next) => {
   try {
-    const intent = _.get(body, "message.intent");
-    const context = _.get(body, "context");
-    const searchMethodNames = _.keys(intent);
-    const catalog = {};
-    // Iterating over all the keys to get the names of search method
-    for (let i = 0; i < searchMethodNames.length; i++) {
-      try {
-        let searchMethodInput = _.get(intent, searchMethodNames[i]);
-        // ... Switching Method Name for Specific Search Criteria
-        // TO-DO Error Handling for Search Method Inputs
-        switch (searchMethodName) {
-          case BPP_CONSTANTS.PROVIDER:
-            catalog["bpp/providers"] = await bpp.search.searchProvider(
-              searchMethodInput
-            );
-            break;
-          case BPP_CONSTANTS.OFFER:
-            catalog["bpp/offers"] = await bpp.search.searchOffers(
-              searchMethodInput
-            );
-            break;
-          case BPP_CONSTANTS.PAYMENT:
-            catalog["bpp/payments"] = await bpp.search.searchPayments(
-              searchMethodInput
-            );
-            break;
-          case BPP_CONSTANTS.CATEGORY:
-            catalog["bpp/categories"] = await bpp.search.searchCategories(
-              searchMethodInput
-            );
-            break;
-          case BPP_CONSTANTS.FULFILLMENT:
-            catalog["bpp/fulfillments"] = await bpp.search.searchFulfillments(
-              searchMethodInput
-            );
-            break;
-          default:
-            console.log(`Invalid Search Method Name ${searchMethodNames[i]}`);
-        }
-      } catch (error) {
-        console.log(`Error Occured ... Skipping ${searchMethodNames[i]}`);
-      }
-    }
-    const response = {
-      message: {
-        catalog,
-      },
-      context,
-    };
-    // Call On Search Method of BAP - TO-DO
+   
     res.status(200).send(response);
   } catch (error) {
     res
