399ed5185b158b23bb77f0ac6e0547807bc797d0
Nikhil Sharma <nikhil.k1012@gmail.com>
2021-08-13 20:45:47 +0530

    Adding Auth API

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const mobilty = require("./mobility");
-
+const auth = (req, res, next) => {
+    // Authentication Verification
+    next();
+}
 // Middleware to add Auth
-router.use("/", mobilty); // Beckn Provider Platform
+router.use("/", auth, mobilty); // Beckn Provider Platform
 
 module.exports = router;
